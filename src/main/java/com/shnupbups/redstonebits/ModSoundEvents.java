edf30a62c35d6d832e2685b1cb1e1685ddd7f935
Shnupbups <shnupbups@gmail.com>
2021-12-28 20:48:05 +1100

    Fix registry error upon joining server

--- src/main/java/com/shnupbups/redstonebits/ModSoundEvents.java
+++ src/main/java/com/shnupbups/redstonebits/ModSoundEvents.java
@@ -5,14 +5,22 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModSoundEvents {
-	public static final SoundEvent BLOCK_COPPER_BUTTON_CLICK_ON = register("block.copper_button.click_on");
-	public static final SoundEvent BLOCK_COPPER_BUTTON_CLICK_OFF = register("block.copper_button.click_off");
-	public static final SoundEvent BLOCK_COUNTER_CLICK = register("block.counter.click");
-	public static final SoundEvent BLOCK_RESISTOR_CLICK = register("block.resistor.click");
-	public static final SoundEvent BLOCK_ADDER_CLICK = register("block.adder.click");
+	public static final SoundEvent BLOCK_COPPER_BUTTON_CLICK_ON = new SoundEvent(RedstoneBits.id("block.copper_button.click_on"));
+	public static final SoundEvent BLOCK_COPPER_BUTTON_CLICK_OFF = new SoundEvent(RedstoneBits.id("block.copper_button.click_off"));
+	public static final SoundEvent BLOCK_COUNTER_CLICK = new SoundEvent(RedstoneBits.id("block.counter.click"));
+	public static final SoundEvent BLOCK_RESISTOR_CLICK = new SoundEvent(RedstoneBits.id("block.resistor.click"));
+	public static final SoundEvent BLOCK_ADDER_CLICK = new SoundEvent(RedstoneBits.id("block.adder.click"));
 
-	public static SoundEvent register(String id) {
-		Identifier identifier = RedstoneBits.id(id);
-		return Registry.register(Registry.SOUND_EVENT, identifier, new SoundEvent(identifier));
+	public static SoundEvent register(SoundEvent event) {
+		Identifier identifier = event.getId();
+		return Registry.register(Registry.SOUND_EVENT, identifier, event);
+	}
+
+	public static void init() {
+		register(BLOCK_COPPER_BUTTON_CLICK_ON);
+		register(BLOCK_COPPER_BUTTON_CLICK_OFF);
+		register(BLOCK_COUNTER_CLICK);
+		register(BLOCK_RESISTOR_CLICK);
+		register(BLOCK_ADDER_CLICK);
 	}
 }
--- src/main/java/com/shnupbups/redstonebits/RedstoneBits.java
+++ src/main/java/com/shnupbups/redstonebits/RedstoneBits.java
@@ -14,6 +14,7 @@ public class RedstoneBits implements ModInitializer {
 	public void onInitialize() {
 		ModBlocks.init();
 		ModBlockEntities.init();
+		ModSoundEvents.init();
 		ModScreenHandlers.init();
 	}
 }
